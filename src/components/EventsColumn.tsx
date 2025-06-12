e489ee877676abd0a4e9be08f791abc9979b2acb
Monil3571 <monilpatel234@gmail.com>
2025-06-12 10:47:05 -0400

    changed the image wrappers in the events column to be fixed width and height

--- src/components/EventsColumn.tsx
+++ src/components/EventsColumn.tsx
@@ -9,11 +9,11 @@ export default function EventsColumn(props: {
     className?: string;
     reverse?: boolean;
 }) {
-    const { events, className, reverse} = props;
+    const { events, className, reverse } = props;
     return (
         <motion.div
             initial={{
-                y:reverse ? "-50%" : 0,
+                y: reverse ? "-50%" : 0,
             }}
             animate={{ y: reverse ? 0 : "-50%" }}
             transition={{
@@ -23,34 +23,33 @@ export default function EventsColumn(props: {
             }}
             className={twMerge("flex flex-col gap-4 pb-4", className)}
         >
-            {Array.from({length:2}).map((_,i)=>(
+            {Array.from({ length: 2 }).map((_, i) => (
                 <Fragment key={i}>
+                    {events.map((event) => (
+                        <div
+                            className="bg-neutral-900 border border-white/10 rounded-3xl p-6"
+                            key={event.name}
+                        >
+                            <div className="w-full h-48 relative rounded-xl overflow-hidden">
+                                <Image
+                                    src={event.icon}
+                                    alt={`${event.name} icon`}
+                                    fill
+                                    className="object-cover"
+                                />
+                            </div>
 
-            {events.map((event) => (
-                <div
-                className="bg-neutral-900 border border-white/10 rounded-3xl p-6"
-                key={event.name}
-                >
-                    <div className="flex justify-center">
-                        <Image
-                            src={event.icon}
-                            alt={"${integration.name} icon"}
-                            className="object-cover"
-                            />
-                    </div>
-                    <h3 className="text-xl mt-6">
-                        {event.name}
-                    </h3>
-                    <p className=" text-white/50 mt-2">
-                        {event.description}
-                    </p>
-                    <p className=" text-white/50 mt-2">
-                        {event.day} {event.time}
-                    </p>
-                </div>
+                            <h3 className="text-xl mt-6">{event.name}</h3>
+                            <p className=" text-white/50 mt-2">
+                                {event.description}
+                            </p>
+                            <p className=" text-white/50 mt-2">
+                                {event.day} {event.time}
+                            </p>
+                        </div>
+                    ))}
+                </Fragment>
             ))}
-            </Fragment>
-        ))}
         </motion.div>
     );
 }
